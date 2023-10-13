a3629fb107c94370cb2d2f3975e652b129a18d38
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-10-13 13:16:12 -0400

    im doin something i think

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const DOMSelectors{
+const DOMSelectors = {
+  test: document.getElementById("test"),
+};
+objectCreation(".form");
+objectCreation("firstName");
+objectCreation("h2");
+
+function objectInjection(object) {
+  DOMSelectors.object == object;
+}
+function formReset(object) {
+  document.getElementById(object).reset();
+}
+function objectRemove(object, arr) {
+  arr = arr.filter((functobject) => functobject.form != { object });
 }
 
-function objectCreation(object){
-
-}
-function objectInjection(object){
-    DOMSelectors.push(object)
-}
-function formReset(object){
-    document.getElementById({object}).reset();
-}
-function objectRemove(object,arr){
-    arr=arr.filter(functobject => functobject.form != 'object')
+function objectCreation(object) {
+  var object = document.getElementById({ object });
+  objectInjection(object);
+  if (object) {
+    DOMSelectors.object.addEventListener("submit"),
+      function (event) {
+        event.preventDefault(); //stps page refresh
+        formReset(object);
+        DOMSelectors.h2.forEach(
+          (h2) => (h2.textContent = DOMSelectors.firstName.value)
+        );
+      };
+  }
 }
