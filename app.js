1b0bb560934fa2b29fe6cb564384873121dd2e15
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-10-17 13:14:10 -0400

    my name? whalen. whapmistelenrn

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 const DOMSelectors = [
   {
     test: document.getElementById("test"),
-    p: document.getElementsByTagName("p"),
+    p: document.getElementById("p"),
   },
 ];
 function injectObject(object) {
   DOMSelectors.push({ object: document.getElementById(object) });
 }
 function clearObjectInput(object) {
-  document.getElementById(object).value = "";
+  document.getElementById(object) == "";
+}
+function updateObject(object) {
+  document.getElementById(object).update();
 }
 function deleteObject(object) {
   delete DOMSelectors.object;
 }
-function createObject() {
-  function $(object) {
-    // cashsign used to replicate jQuery format
-    this.object = document.getElementById(object);
-    injectObject(object);
-    clearObjectInput(object);
-  }
+const reloadObject = (event) => {};
+function createObject(object) {
+  object = document.getElementById(object);
+  injectObject(object);
+  clearObjectInput(object);
 }
-
+console.log(DOMSelectors);
+createObject("p");
+console.log(DOMSelectors);
 //https://www.w3schools.com/js/js_htmldom_elements.asp
+
+//object.update,getelementbyid(object).update()
+
+/*   object.onbeforeunload(() => {
+    before the page unloads it refreshes
+    object.reload();
+  })''
+  */
+
+//research dom injector
