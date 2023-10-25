e1a078bbf23bc65b55eb1f95fa79a7927693e5e6
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-10-25 13:14:05 -0400

    ,

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const DOMSelectors = {
-  form: document.querySelector(form),
-  button: document.querySelector(button),
-  card: document.querySelector(card),
-  cardlist: document.getElementsByClassName(listOfCards),
-};
+const DOMSelectors = [
+  {
+    form: document.querySelector("form"),
+    button: document.querySelector(".button"),
+    card: document.querySelector("card"),
+    cardlist: document.querySelector(".listOfCards"),
+  },
+];
 
 function clearInputField(object) {
   // clears object
@@ -12,25 +14,26 @@ function clearInputField(object) {
 }
 function removeObject(object) {
   //removes object from DOMSelectors
-  // removes object
-  delete DOMSelectors.object;
+  delete DOMSelectors.object.remove;
 }
 function addObject(object) {
   //adds object to DOMSelectors
   DOMSelectors.push({
-    key: object,
-    value: object.value,
+    key: `${object}`,
+    value: document.querySelector(`.${object}`),
   });
 }
 function createObject(object) {
   // creates object
-  const object = document.querySelector({ object });
+  object = document.querySelector(`.${object}`);
   addObject(object);
   clearInputField(object);
 }
 DOMSelectors.button.addEventListener("click", function (event) {
   event.preventDefault;
   let form = DOMSelectors.form.value; // don't forget .value
-  createObject(card);
-  DOMSelectors.cardlist.insertAdjacentHTML("afterbegin", `<p> ${card} </p>`);
+  createObject("card");
+  DOMSelectors.cardlist.insertAdjacentHTML("afterbegin", `<p> ${card}</p>`);
+  DOMSelectors.card.innerHTML(`${form}`);
 });
+//create img,text,id, add adjacet html when submitted. fix functions mayb
