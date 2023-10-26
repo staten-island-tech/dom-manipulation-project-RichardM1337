f05cff486c0769fc0708049863d1f07e22368f8b
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-10-26 13:13:33 -0400

    easy project

--- app.js
+++ app.js
@@ -16,36 +16,32 @@ function removeObject(objectKey) {
   delete DOMSelectors[objectKey];
 }
 
-function addObject(objectKey, objectValue) {
+function addObject(objectName, objectClassName) {
   // adds property to DOMSelectors
-  DOMSelectors[objectKey] = objectValue; // bracket notation lets you access code dynamically
+  const objectQuery = document.querySelector(`.${objectClassName}`); // tells dom where it is
+  DOMSelectors[objectName] = [objectQuery];
 }
 
-function createObject(objectClassName) {
+function createObject(objectName, objectClassName) {
   // creates and adds an object to DOMSelectors
-  const newObject = document.querySelector(`.${objectClassName}`);
-  addObject(objectClassName, newObject);
-  clearInputField(newObject);
+  addObject(objectName, objectClassName); // adds it to DOMSelectors
+  clearInputField(objectName);
 }
-
 DOMSelectors.button.addEventListener("click", function (event) {
   event.preventDefault();
   const inputValue = DOMSelectors.input.value; // don't forget .value
-  createObject("card");
-  DOMSelectors.cardlist.insertAdjacentHTML(
-    "afterbegin",
-    `<p>${inputValue}</p>`
-  );
-});
-/*
-const inputValue = DOMSelectors.input.value; // don't forget .value
-  createObject("card");
-  DOMSelectors.cardlist.insertAdjacentHTML(
-    "afterbegin",
-    `<p>${inputValue}</p>`
-  );
+  const card = document.createElement("li");
+  card.innerHTML = `<p>${inputValue}</p>
+    <button type="button" class="delete-button">delete this card</button>`;
+  DOMSelectors.cardlist.appendChild(card);
+  clearInputField(DOMSelectors.input);
 });
 
+createObject("delete-button", "delete-button");
+DOMSelectors.delete.addEventListener("click", function (event) {
+  const parentCard = event.target.parentElement;
+  DOMSelectors.cardlist.removeChild(parentCard);
+});
 
-if (DOMSelectors.card) {
-    DOMSelectors.card.textContent = formValue; // use textContent instead of innerHTML for plain text*/
+// agenda
+// do not accept empty forms
